2a5f8be59b7c89aa52990df3ecc47a48b1c531be
kylerlittle <kyler.little@wsu.edu>
2018-10-06 19:16:41 -0700

    tested and done

--- labs/lab-4/app.c
+++ labs/lab-4/app.c
@@ -13,29 +13,30 @@ int run_cp_simulator(int argc, char* argv[]) {
 int myrcp(char *f1, char *f2)
 {
     struct stat st1, st2;
-    int result;
+    int r1, r2;
 
     /* 1a. If f1 doesn't exist, reject. */
-    if ((result = stat(f1, &st1)) == ERROR_CODE) {
+    if ((r1 = stat(f1, &st1)) == ERROR_CODE) {
         printf("%s doesn't exist\n", f1);
         exit(1);
     }
-    result = stat(f2, &st2);
+    r2 = stat(f2, &st2);
+    // BELOW STUFF -- from KC Wang's pseudocode, but it doesn't make any logical sense to do this, so commented out
     // /* 1b. f1 exists; if f1 is neither regular nor link, reject. */
-    // if (!S_ISREG(st1.st_mode) || S_ISLNK(st1.st_mode)) {
+    // if (!S_ISREG(st1.st_mode) || !S_ISLNK(st1.st_mode)) {
     //     printf("%s is neither regular nor link\n", f1);
     //     exit(1);
     // }
-    // /* 1c. If f2 exists and isn't a regular or is a link file, reject. */
-    // if ((result = stat(f2, &st2)) != ERROR_CODE && (!S_ISREG(st1.st_mode) || S_ISLNK(st1.st_mode))) {
+    // /* 1c. If f2 exists and isn't a regular or a link file, reject. */
+    // if ((result = stat(f2, &st2)) != ERROR_CODE && (!S_ISREG(st1.st_mode) || !S_ISLNK(st1.st_mode))) {
     //     printf("%s exists but isn't regular/link file\n", f2);
     //     exit(1);
     // }
     
     /* 2. If f1 is a regular file, continue processing. */
     if (S_ISREG(st1.st_mode)) {
-        /* If f2 (recall result refers to file 2) doesn't exist or exists and is regular file. */
-        if (result == ERROR_CODE || (result != ERROR_CODE && S_ISREG(st2.st_mode))) {
+        /* If f2 doesn't exist or exists and is regular file. */
+        if (r2 == ERROR_CODE || (r2 != ERROR_CODE && S_ISREG(st2.st_mode))) {
             // when we call cpf2f here, f2 may not exist...
             return cpf2f(f1, f2);
         } else { /* f2 exists and is a directory */
@@ -46,13 +47,13 @@ int myrcp(char *f1, char *f2)
     /* 3. If f1 is a directory file, continue processing. */
     if (S_ISDIR(st1.st_mode)) {
         /* If f2 exists and isn't a directory, we have a problem. Can't copy dir into file. */
-        if (result != ERROR_CODE && !S_ISDIR(st2.st_mode)) {
+        if (r2 != ERROR_CODE && !S_ISDIR(st2.st_mode)) {
             printf("Cannot copy dir %s into file %s\n", f1, f2);
             exit(1);
         }
-        if (result == ERROR_CODE) { /* Doesn't exist, so make it, then recurse. */
-            /* Use same permissions as f1. */
-            if ((result = mkdir(f2, st1.st_mode)) == ERROR_CODE) {
+        if (r2 == ERROR_CODE) { /* Doesn't exist, so make it, then recurse. */
+            /* Use same permissions as f1. Also just reuse r2. Might as well. */
+            if ((r2 = mkdir(f2, st1.st_mode)) == ERROR_CODE) {
                 printf("%s doesn't exist; tried to create it as directory and failed\n", f2);
                 exit(1);
             }
@@ -63,36 +64,32 @@ int myrcp(char *f1, char *f2)
 
 int cpf2f(char *f1, char *f2)
 {
-    //   1. reject if f1 and f2 are the SAME file
+    
     struct stat st1, st2;
     int r1, r2, fd1, fd2 = INT_MAX, n, total = 0;
     char buf[BLOCK_SIZE];
 
-    r1 = stat(f1, &st1);
-    r2 = stat(f2, &st2);
+    /* Gotta use lstat here so we don't follow the links. */
+    r1 = lstat(f1, &st1);
+    r2 = lstat(f2, &st2);
+
+    /* 1. Reject if f1 and f2 are the SAME file. */
     if (st1.st_ino == st2.st_ino && st1.st_dev == st2.st_dev) {
         printf("%s and %s are the same file\n", f1, f2);
         exit(1);
     }
-    //   2. if f1 is LNK and f2 exists: reject
+    /* 2. Reject if f1 is a link and f2 exists */
     if (S_ISLNK(st1.st_mode) && r2 != ERROR_CODE) {
         printf("%s is link and %s exists\n", f1, f2);
         exit(1);
     }
-    //   3. if f1 is LNK and f2 does not exist: create LNK file f2 SAME as f1
+    /* 3. If f1 is a link and f2 does not exist: create link file f2 (SAME as f1). */
     if (S_ISLNK(st1.st_mode) && r2 == ERROR_CODE) {
-        fd2 = open(f2, O_WRONLY|O_CREAT|O_TRUNC, st1.st_mode);
-        if (fd2 < 0) {
-            printf("error creating %s\n", f2);
-            exit(1);        // exit on error
-        }
+        return link(f1, f2);
     }
+    /* OTHERWISE, open f1 for read, f2 for write (and overwrite). */
     fd1 = open(f1, O_RDONLY);
-    if (fd2 == INT_MAX) fd2 = open(f2, O_WRONLY|O_CREAT|O_TRUNC, st1.st_mode);
-    //   4:
-    //      open f1 for READ;
-    //      open f2 for O_WRONLY|O_CREAT|O_TRUNC, mode=mode_of_f1;
-    //      copy f1 to f2
+    fd2 = open(f2, O_WRONLY|O_CREAT|O_TRUNC, st1.st_mode);
     while ((n = read(fd1, buf, BLOCK_SIZE))) {
         write(fd2, buf, n);
         total += n;
@@ -107,11 +104,7 @@ int cpf2d(char *f1, char *f2)
 {
     char _basename[MAX_FILENAME_LEN], result[MAX_FILENAME_LEN], _slash[MAX_FILENAME_LEN] = "/";
 
-    /* Get full file path -- f2/_basename. Store in result. */
-    // strcpy(_basename, basename(f1));
-    // strcpy(result, f2);
-    // strcat(_slash,_basename);
-    // strcat(result, _slash);
+    /* Get full file path -- f2/basename(f1). Store in result. */
     f2slashbasef1(f1, f2, result);
     if (DEBUG_MODE) printf("Copying %s to %s\n", f1, result);
 
@@ -141,7 +134,7 @@ int cpd2d(char *f1, char *f2)
 
     /* First, we check if f2 dir exists. If not, create it. */    
     if ((r2 = stat(f2, &st2)) == ERROR_CODE) {
-        r1 = stat(f1, &st1);   // we'll just assume this succeeds
+        r1 = stat(f1, &st1);   // it's very reasonable to assume this succeeds
         if ((result = mkdir(f2, st1.st_mode)) == ERROR_CODE) {
             printf("%s doesn't exist; tried to create it as directory and failed\n", f2);
             exit(1);
@@ -170,24 +163,14 @@ int cpd2d(char *f1, char *f2)
         /* If f (i.e. f1/filename) is a regular file, simply call cpf2d. */
         if (S_ISREG(current_f_st.st_mode)) cpf2d(full_f1_path, f2);
         /* If f (i.e. f1/filename) is a directory and NOT ".." or ".", recurse. */
-        // if (strncmp(_dirent->d_name, ".", 1)) printf("_dirent->name: %s\n", _dirent->d_name);
-        if (S_ISDIR(current_f_st.st_mode) && strncmp(_dirent->d_name, ".", 1)) cpd2d(full_f1_path, full_f2_path);
-        // if (result != ERROR_CODE) {
-        //     printf("mode is %d\n", current_f_st.st_mode);
-        // }
-        // printf("f->%s\n", _dirent->d_name);
+        if (S_ISDIR(current_f_st.st_mode) && strcmp(_dirent->d_name, ".") && strcmp(_dirent->d_name, ".."))
+            cpd2d(full_f1_path, full_f2_path);
     }
     closedir(_dir);
 }
 
 int f2slashbasef1(char *f1, char *f2, char *result) {
-    // char _basename[MAX_FILENAME_LEN], result[MAX_FILENAME_LEN], _slash[MAX_FILENAME_LEN] = "/";
-
-    // /* Get full file path -- f2/_basename. Store in result. */
-    // strcpy(_basename, basename(f1));
-    // strcpy(result, f2);
-    strcpy(result, f2);
-    strcat(result, "/");
-    strcat(result, basename(f1));
-    // strcat(result, _slash);
+    strcpy(result, f2);             // result = f2
+    strcat(result, "/");            // result = f2/
+    strcat(result, basename(f1));   // result = f2/basename(f1)
 }
